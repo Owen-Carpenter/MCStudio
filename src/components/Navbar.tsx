7bdafe416de6facd049a485eb30b4a70e62776fb
Owen-Carpenter <owen.carpenter.work@gmail.com>
2025-01-08 14:14:16 -0600

    Navbar is now responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,49 @@
 import "../Styles/Navbar.css"
 import { Link } from "react-router-dom";
+import Hamburger from 'hamburger-react'
+import { useState } from "react";
 
 function Navbar() {
-  return <>
-    <section className="navbar-container">
-        <div className="logo-section">
-            <a href=""><img src="/public/assets/mcstudioLogo.png" alt="" className="logo" /></a>
-        </div>
-        <nav className="navbar">
-            <Link className="links" to={"/"}>Home</Link>
-            <Link className="links" to={"/Services"}>Services</Link>
-            <Link className="links" to={"/"}>Gallery</Link>
-            <Link className="links" to={"/About"}>About</Link>
-            <Link className="links" to={"/Contact"}>Contact</Link>
+
+  const [isOpen, setOpen] = useState(false)
+
+  return (
+    <>
+      <section className={`navbar-container ${isOpen ? "expanded" : ""}`}>
+        <div className="top-bar">
+          <div className="logo-section">
+            <a href="">
+              <img src="/public/assets/mcstudioLogo.png" alt="" className="logo" />
+            </a>
+          </div>
+          <div className="top-bar-menu">
             <Link to={"/Account"}>
-                <button className="login">Login</button>
+              <button className="login">Login</button>
             </Link>
-            <Link className="links" to={"/Purse"}><img src="/assets/purse.png" alt="" className="purse" /></Link>
+            <Link className="links" to={"/Purse"}>
+              <img src="/assets/purse.png" alt="" className="purse" />
+            </Link>
+            <button className="menu">
+                <Hamburger toggled={isOpen} toggle={setOpen} />
+            </button>
+          </div>
+        </div>
+        <nav className={`navbar ${isOpen ? "open" : ""}`}>
+          <Link className="links" to={"/"}>Home</Link>
+          <Link className="links" to={"/Services"}>Services</Link>
+          <Link className="links" to={"/Gallery"}>Gallery</Link>
+          <Link className="links" to={"/About"}>About</Link>
+          <Link className="links" to={"/Contact"}>Contact</Link>
+          <Link to={"/Account"}>
+            <button className="login">Login</button>
+          </Link>
+          <Link className="links" to={"/Purse"}>
+            <img src="/assets/purse.png" alt="" className="purse" />
+          </Link>
         </nav>
-    </section>
-  </>;
+      </section>
+    </>
+  );
 }
 
 export default Navbar;
--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,7 +1,9 @@
+import Navbar from "../components/Navbar"
+
 export function Gallery(){
     return(
         <>
-            
+            <Navbar />
         </>
     )
 }
