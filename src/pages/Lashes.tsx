5d65fa02ec15d0f5130bb4e2a44891460e264e61
Owen-Carpenter <owen.carpenter.work@gmail.com>
2025-01-15 08:57:48 -0600

    Page creation for services

---
+++ src/pages/Lashes.tsx
@@ -0,0 +1,7 @@
+export function Lashes(){
+    return(
+        <>
+            
+        </>
+    );
+}
---
+++ src/pages/Other.tsx
@@ -0,0 +1,7 @@
+export function Other(){
+    return(
+        <>
+            
+        </>
+    );
+}
