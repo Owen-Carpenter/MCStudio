ee39e9b3b9c52364022203a8ec1065e60e54c070
Owen-Carpenter <owen.carpenter.work@gmail.com>
2025-01-19 12:33:13 -0600

    Service cards styling complete. Service card params for component complete. Npm start dependencies updated.

--- client/src/components/DetailedServiceCard.tsx
+++ client/src/components/DetailedServiceCard.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import '../styles/DetailedServicesCard.css'
+import '../styles/DetailedServicesCard.css';
 import { Link } from 'react-router-dom';
 
-function DetailedServiceCard() {
+function DetailedServiceCard({title, desc, price, time, imgURL}: {title: string, desc: string, price: number, time: string, imgURL : string}) {
 
     const handleAddToCart = () => {
-    
+
     }
 
   return (
     <>
       <div className="detailed-card">
         <div className="detailed-card-img">
-            <img src="/assets/angel-wet-icon.png" alt="" />
+            <img src={imgURL} alt="" />
         </div>
         <div className="detailed-card-content">
             <div className="detailed-card-text">
-                <h1>Lash Lift & Tint</h1>
-                <p>With a lash lift and tint, you can say goodbye to  curling lashes and wearing mascara. This treatment lasts 4-6 weeks depending on aftercare and lifestyle.</p>
+                <h1>{title}</h1>
+                <p>{desc}</p>
             </div>
             <div className="detailed-card-price">
-                <h2>$70</h2>
-                <Link to={'/purse'}><button className="atc-btn">Add to Purse</button></Link>
-                <h2>150 Min</h2>
+                <h2>${price}</h2>
+                <Link to={'/purse'}><button className="atc-btn"><img src="/assets/atc-icon.png" alt="" /></button></Link>
+                <h2>{time}</h2>
             </div>
         </div>
       </div>
--- client/src/pages/Facials.tsx
+++ client/src/pages/Facials.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import "../styles/Facials.css";
 import DetailedServiceCard from "../components/DetailedServiceCard";
+import Footer from "../components/Footer";
 
 export function Facials(){
     return(
@@ -9,14 +10,37 @@ export function Facials(){
             <section className="facials-services-container">
                 <h1 className="facials-services-title">Facials Offered</h1>
                 <div className="card-container">
-                    <DetailedServiceCard />
-                    <DetailedServiceCard />
-                    <DetailedServiceCard />
-                    <DetailedServiceCard />
-                    <DetailedServiceCard />
-                    <DetailedServiceCard />
+                    <DetailedServiceCard
+                        imgURL="/assets/dermaplane-icon.png"
+                        title="Mini Dermaplane Facial"
+                        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        price={40}
+                        time="40 Min"
+                    />
+                    <DetailedServiceCard
+                        imgURL="/assets/aha-icon.png"
+                        title="AHA Chemical Peel"
+                        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        price={60}
+                        time="45 Min"
+                    />
+                    <DetailedServiceCard
+                        imgURL="/assets/procell-icon.png"
+                        title="Procell Therapies Treatment"
+                        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        price={350}
+                        time="45 Min"
+                    />
+                    <DetailedServiceCard
+                        imgURL="/assets/brow-lamination-icon.png"
+                        title="Brow Lamination"
+                        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        price={75}
+                        time="60 Min"
+                    />
                 </div>
             </section>
+            <Footer />
         </>
     );
 }
