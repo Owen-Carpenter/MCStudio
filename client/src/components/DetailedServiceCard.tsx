2ca50cbcacb1d4e97fcf5059918b0ebae43f833e
Owen-Carpenter <owen.carpenter.work@gmail.com>
2025-01-18 22:08:29 -0600

    Start of the DetailedServiceCard component

---
+++ client/src/components/DetailedServiceCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/DetailedServicesCard.css'
+
+function DetailedServiceCard() {
+  return (
+    <>
+      <div className="detailed-card">
+        asdf
+      </div>
+    </>
+  );
+}
+
+export default DetailedServiceCard;
--- client/src/pages/Facials.tsx
+++ client/src/pages/Facials.tsx
@@ -1,9 +1,23 @@
 import Navbar from "../components/Navbar";
+import "../styles/Facials.css";
+import DetailedServiceCard from "../components/DetailedServiceCard";
 
 export function Facials(){
     return(
         <>
             <Navbar />
+            <section className="facials-services-container">
+                <h1 className="facials-services-title">Facials Offered</h1>
+                <div className="card-container">
+                    <DetailedServiceCard />
+                    <DetailedServiceCard />
+                    <DetailedServiceCard />
+                    <DetailedServiceCard />
+                    <DetailedServiceCard />
+                    <DetailedServiceCard />
+                    
+                </div>
+            </section>
         </>
     );
 }
